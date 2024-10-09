225cc721c4b10516d84743e7eb4b475e5d978079
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-10-09 02:54:15 +0000

    wip: installs `pg` and creates initial `database.js` module, sets up env vars

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,8 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as Result;");
+  console.log("Database connection test result: ", result);
+
   res.status(200).json({ version: "v1" });
 }
