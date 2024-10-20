72f618bfc876d59a7401ed4d71664d85911636f8
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-10-20 18:02:51 +0000

    wip: moves `client.connect()` inside TryCatch block and adds a log

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,22 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("[database] Local database credentials: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
 
     return result.rows;
   } catch (error) {
     console.error("[database] Query execution FAILED: ", error);
+    throw error;
   } finally {
     await client.end();
   }
