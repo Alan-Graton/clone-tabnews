95cf7bf2b4614df1209fbf55b56409701b846421
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-10-20 01:20:20 +0000

    wip: finish `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,23 @@
 import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as Result;");
-  console.log("Database connection test result: ", result);
+  const updatedAt = new Date().toISOString();
 
-  res.status(200).json({ version: "v1" });
+  const [dbVersion] = await database.query("SHOW server_version;");
+  const [dbMaxConnections] = await database.query("SHOW max_connections;");
+  const [dbOpenConnections] = await database.query({
+    text: "SELECT count(*)::int as open_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion.server_version,
+        max_connections: parseInt(dbMaxConnections.max_connections),
+        open_connections: dbOpenConnections.open_connections,
+      },
+    },
+  });
 }
