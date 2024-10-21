ac20ae9a14b1a7c45e8d0eb2446e30d00f11ed13
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-10-21 12:42:08 +0000

    fix: adjusts postgresql version check

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
   expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toContain("16.");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
