d1a9256dde797932e1de65c54c7ffbdfc55ab275
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-10-21 12:40:04 +0000

    wip: removes creds logs and sets production db to use ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
-  console.log("[database] Local database credentials: ", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+  console.log("CURRENT ENVIRONMENT: ", process.env.NODE_ENV);
 
   try {
     await client.connect();
